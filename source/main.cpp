b75af4b2a80fdecbfb4fdadcf4654f8191c6132f
KuvaevM <shlyabom@gmail.com>
2022-06-02 21:17:21 +0300

    add background

--- source/main.cpp
+++ source/main.cpp
@@ -1,10 +1,13 @@
 #include "constants.h"
 #include "menu/users_menu.h"
+#include "sound/background_music.h"
 
 int main() {
     sf::RenderWindow window(
         sf::VideoMode(Tanks::WINDOW_WIDTH, Tanks::WINDOW_HEIGHT), "Just Tanks",
         sf::Style::Fullscreen);  // create window
+    Tanks::Sound::BackgroundMusicHolder backgroundMusic(musicPath);
+    backgroundMusic.play(100);
     Tanks::Menu::showUsersMenu(window);
     return 0;
 }
