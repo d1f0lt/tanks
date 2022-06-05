80c465aca6133f1f72df870ca3751e3409c1e805
ilma4 <ilya.malakhov4@gmail.com>
2022-06-05 11:23:25 +0300

    Server setup

--- source/main.cpp
+++ source/main.cpp
@@ -6,10 +6,24 @@
 int main(int argc, const char *argv[]) {
     if (argc >= 3 && std::string(argv[1]) == "server") {
         int players = std::stoi(argv[2]);
+        int bots = 10;
+        int bonuses = 2;
         if (players <= 0) {
             return 123;
         }
-        Tanks::createAndRunServer(1, 2);
+        if (argc >= 4) {
+            bots = std::stoi(argv[3]);
+            if (bots < 0) {
+                return 123;
+            }
+        }
+        if (argc >= 5) {
+            bonuses = std::stoi(argv[4]);
+            if (bonuses < 0) {
+                return 123;
+            }
+        }
+        Tanks::createAndRunServer(1, 2, bots, bonuses);
     }
     static const std::string musicPath = "../sounds/background_music.ogg";
     sf::RenderWindow window(
